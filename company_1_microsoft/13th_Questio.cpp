b188e599cc9746ec76c7287d05ac96261290945d
Vishnu <ganeshanvishnu@gmail.com>
2023-01-05 11:17:20 +0530

    more microsoft questions solved

---
+++ company_1_microsoft/13th_Questio.cpp
@@ -0,0 +1,16 @@
+/*question details
+name: Seat Arrangement in a SpiceJet Problem - Online OA February ‘22
+link: https://leetcode.com/problems/airplane-seat-assignment-probability/
+submission link: https://leetcode.com/problems/airplane-seat-assignment-probability/submissions/871701968/
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1) return 1.00000;
+        return 0.50000;
+    }
+};
