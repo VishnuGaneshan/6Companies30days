5c7f53a9f07b120e11f0f5089ae230bb6fc03eac
Vishnu <ganeshanvishnu@gmail.com>
2023-01-14 17:47:35 +0530

    more adobe questions solved

---
+++ company_3_adobe/14th_Question.cpp
@@ -0,0 +1,29 @@
+/*question details
+name: People aware of secret
+link: https://leetcode.com/problems/number-of-people-aware-of-a-secret/
+submission link: https://leetcode.com/problems/number-of-people-aware-of-a-secret/submissions/877958383/
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<long> day(1001, 0);
+        int d = 1;  
+        day[d] = 1;
+        int MOD = 1e9+7;
+        while(d <= n) {
+            for(int j = d + delay; j <= min(d + forget - 1, n); j++) {
+                day[j] = (day[j] + day[d]) % MOD;
+            }
+            d++;
+        }
+        long res = 0;
+        for(int i = max(n - forget + 1, 0); i <= n; i++) {
+            res = (res + day[i]) % MOD;
+        }
+        return res;
+    }
+};
